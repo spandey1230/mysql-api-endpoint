09b7206944234e608c1bf9dd8d644fe008444dc3
Eddson Patrick <edson.patricio.39@gmail.com>
2019-04-22 09:27:29 +0200

    Improving security with escape string

--- endpoint/QueryBuilder.php
+++ endpoint/QueryBuilder.php
@@ -1,14 +1,14 @@
 <?php 
 
-
 class QueryBuilder
 {
 	function qb_select($request){
 		$table = $request['tbl'];
-
+		/*$this->qb_escape_string($conn, );*/
 		$sql = "SELECT ";
 		if(!empty($request['col'])){
 			foreach ($request['col'] as $item) {
+				$item = $this->qb_escape_string($item);
 				$sql = $sql."$item, ";
 			}
 			$sql = rtrim($sql,", ");
@@ -20,6 +20,7 @@ class QueryBuilder
 		$sql = $sql." FROM ";
 
 		foreach ($request['tbl'] as $item) {
+			$item = $this->qb_escape_string($item);
 			$sql = $sql."$item, ";
 		}
 		$sql = rtrim($sql,", ");
@@ -32,20 +33,20 @@ class QueryBuilder
 		
 
 		if(isset($request['ordby'])){
-			$sql = $sql." ORDER BY `".$request['ordby']."`";
+			$sql = $sql." ORDER BY `".$this->qb_escape_string($request['ordby'])."`";
 			/*echo $sql;*/
 		}
 
 		if(isset($request['ord'])){
-			$sql = $sql." ".$request['ord'];
+			$sql = $sql." ".$this->qb_escape_string($request['ord']);
 			/*echo $sql;*/
 		}
 
 		if(isset($request['limit'])){
-			$sql = $sql." LIMIT ".$request['limit'];
+			$sql = $sql." LIMIT ".$this->qb_escape_string($request['limit']);
 			/*echo $sql;*/
 		}
-		echo $sql."<br>";
+		#echo $sql."<br>";
 		$sql = $sql.";";
 		return $sql;
 	}
@@ -54,12 +55,14 @@ class QueryBuilder
 		$sql = " WHERE ";
 
 		if(isset($request['between'])){
-			$sql = $sql." ".$request['between'][0]." BETWEEN ".$request['between'][1]." AND ".$request['between'][2];
+			$sql = $sql." ".$this->qb_escape_string($request['between'][0])." BETWEEN ".$this->qb_escape_string($request['between'][1])." AND ".$this->qb_escape_string($request['between'][2]);
 			/*echo $sql;*/
 		}
 		$sql = rtrim($sql, "AND ");
 
 		foreach ($request['cond'] as $key => $value) {
+			$key = $this->qb_escape_string($key);
+			$value = $this->qb_escape_string($value);
 			$sql = $sql." $key = '$value' AND ";
 		}
 		$sql = rtrim($sql, "AND ");
@@ -70,16 +73,20 @@ class QueryBuilder
 
 	/* Insert into table */
 	function qb_insert($request) {
-		$table = $request['tbl'];
+		$table = $this->qb_escape_string($request['tbl']);
 
 		$sql = "INSERT INTO `$table`(";
 
 		foreach ($request['val'] as $key => $value) {
+			$key = $this->qb_escape_string($key);
+			$value = $this->qb_escape_string($value);
 			$sql = $sql."`$key`, ";
 		}
 		$sql = rtrim($sql,", ");
 		$sql = $sql.") VALUES (";
 		foreach ($request['val'] as $key => $value) {
+			$key = $this->qb_escape_string($key);
+			$value = $this->qb_escape_string($value);
 			$sql = $sql."'$value',";
 		}
 		$sql = rtrim($sql,",");
@@ -91,16 +98,22 @@ class QueryBuilder
 
 	/* Update table */
 	function qb_update($request) {
-		$table = $request['tbl'];
+		$table = $this->qb_escape_string($request['tbl']);
 
 		$sql = "UPDATE `$table` SET ";
 
 		foreach ($request['col'] as $key => $value) {
+			$key = $this->qb_escape_string($key);
+			$value = $this->qb_escape_string($value);
+
 			$sql = $sql."`$key`='$value', ";
 		}
 		$sql = rtrim($sql,", ");
 		$sql = $sql." WHERE ";
 		foreach ($request["cond"] as $key => $value) {
+			$key = $this->qb_escape_string($key);
+			$value = $this->qb_escape_string($value);
+
 			$sql = $sql."`$key`='$value' AND ";
 		}
 		$sql = rtrim($sql,"AND ");
@@ -112,11 +125,14 @@ class QueryBuilder
 
 	/* Update table */
 	function qb_delete($request) {
-		$table = $request['tbl'];
+		$table = $this->qb_escape_string($request['tbl']);
 
 		$sql = "DELETE FROM `$table` WHERE ";
 
 		foreach ($request["cond"] as $key => $value) {
+			$key = $this->qb_escape_string($key);
+			$value = $this->qb_escape_string($value);
+			
 			$sql = $sql."`$key`='$value' AND ";
 		}
 		$sql = rtrim($sql,"AND ");
@@ -124,5 +140,24 @@ class QueryBuilder
 		# echo $sql;
 		return $sql;
 	}
-	/* end Update table */	
+	/* end Update table */
+
+	/* escape string */
+	function qb_escape_string($string) {
+		require("../config.php");
+		
+		$username = $config['db']['username'];
+		$password = $config['db']['password'];
+		$host = $config['db']['host'];
+		$port = $config['db']['port'];
+		$database = $config['db']['database'];
+
+		$conn = new PDO("mysql:host=$host;dbname=$database;port=$port;", $username, $password);
+
+		$str = $conn->quote($string);
+		$str = rtrim($str, "'");
+		$str = substr($str, 1);
+		return $str;
+	}
+	/* end escape string */
 }
